a81d46d36011d18d19e1791dc3941f9c198b5dbc
aravindpanil <aravindpanil@gmail.com>
2018-06-27 19:24:53 +0530

    Reformat signup.js

--- ee/ee/account/signup/signup.js
+++ ee/ee/account/signup/signup.js
@@ -2,21 +2,21 @@
 'use strict';
 
 var config = browser.params;
-var subs=require('../common/subSegment');
+var subs = require('../common/subSegment');
 
 
 describe('Sign up', function () {
 
-    var data,signup;
+    var data, signup;
 
-    data=require('./signupdata');
-    signup=require('../common/sign.common');
-    var generateOTP=element(by.xpath('//button[@aria-label=\'Generate OTP\']'));
-    var otp=element(by.model('otp'));
-    var nxt=element(by.xpath('//button[@aria-label=\'Next\' and @aria-hidden=\'false\']'));
-    var start=element(by.xpath('//button[@aria-label=\'Start your 45 days free trial\']'));
-    var username=element(by.xpath('//input[@aria-label=\'Email/Mobile No.\']'));
-    var password=element(by.model('password'));
+    data = require('./signupdata');
+    signup = require('../common/sign.common');
+    var generateOTP = element(by.xpath('//button[@aria-label=\'Generate OTP\']'));
+    var otp = element(by.model('otp'));
+    var nxt = element(by.xpath('//button[@aria-label=\'Next\' and @aria-hidden=\'false\']'));
+    var start = element(by.xpath('//button[@aria-label=\'Start your 45 days free trial\']'));
+    var username = element(by.xpath('//input[@aria-label=\'Email/Mobile No.\']'));
+    var password = element(by.model('password'));
 
     beforeEach(function () {
         browser.get('http://localhost:3000/#!/register');
@@ -28,48 +28,48 @@ describe('Sign up', function () {
 
 
     function getsubSegment(seg) {
-        if(seg === 'Cereals')
+        if (seg === 'Cereals')
             return subs.Cereals;
-        else if(seg === 'Coffee')
+        else if (seg === 'Coffee')
             return subs.Coffee;
-        else if(seg === 'Vegetables')
+        else if (seg === 'Vegetables')
             return subs.Vegetables;
-        else if(seg === 'Yarn')
+        else if (seg === 'Yarn')
             return subs.Yarn;
-        else if(seg === 'Fabric')
+        else if (seg === 'Fabric')
             return subs.Fabric;
-        else if(seg === 'Apparel')
+        else if (seg === 'Apparel')
             return subs.Apparel;
     }
 
 
-    function getsubSegmentData(seg,subseg) {
-        if(seg === 'Cereals' && subseg.Cereals)
+    function getsubSegmentData(seg, subseg) {
+        if (seg === 'Cereals' && subseg.Cereals)
             return subseg.Cereals;
-        else if(seg === 'Coffee' && subseg.Coffee)
+        else if (seg === 'Coffee' && subseg.Coffee)
             return subseg.Coffee;
-        else if(seg === 'Vegetables' && subseg.Vegetables)
+        else if (seg === 'Vegetables' && subseg.Vegetables)
             return subseg.Vegetables;
-        else if(seg === 'Yarn' && subseg.Yarn)
+        else if (seg === 'Yarn' && subseg.Yarn)
             return subseg.Yarn;
-        else if(seg === 'Fabric' && subseg.Fabric)
+        else if (seg === 'Fabric' && subseg.Fabric)
             return subseg.Fabric;
-        else if(seg === 'Apparel' && subseg.Apparel)
+        else if (seg === 'Apparel' && subseg.Apparel)
             return subseg.Apparel;
         return 0;
     }
 
-    function exploreMore(seg,subseg) {
+    function exploreMore(seg, subseg) {
         var segs = ['Cereals', 'Coffee', 'Vegetables', 'Yarn', 'Fabric', 'Apparel'];
-        for(var k = 0; k < seg.length; k++){
+        for (var k = 0; k < seg.length; k++) {
             var flag = 0;
             for (var i = 0; i < segs.length; i++) {
                 if (seg[k] === segs[i]) {
                     flag = 1;
                     if (subseg) {
                         var sub = getsubSegment(seg[k]);
-                        var subsegdata = getsubSegmentData(seg[k],subseg);
-                        if(subsegdata === 0)
+                        var subsegdata = getsubSegmentData(seg[k], subseg);
+                        if (subsegdata === 0)
                             return false;
                         for (var j = 0; j < subsegdata.length; j++) {
                             var flag1 = 0;
@@ -84,14 +84,14 @@ describe('Sign up', function () {
                     }
                 }
             }
-            if(flag === 0)
+            if (flag === 0)
                 return false;
         }
         return true;
     }
 
 
-    function validateSegment(trad,seg,subseg) {
+    function validateSegment(trad, seg, subseg) {
         if (seg) {
             var segs = ['Rice', 'Coffee', 'Textiles', 'Fruits and Vegetables'];
 
@@ -118,13 +118,13 @@ describe('Sign up', function () {
                             break;
                         }
                     }
-                    if(flag === 0)
+                    if (flag === 0)
                         return exploreMore(seg, subseg);
                 }
                 return true;
             }
             else if (trad === 'Retailer') {
-                    return exploreMore(seg, subseg);
+                return exploreMore(seg, subseg);
             }
             else
                 return false;
@@ -134,7 +134,7 @@ describe('Sign up', function () {
 
 
 
-    function selectSegment(trad,seg,subseg) {
+    function selectSegment(trad, seg, subseg) {
         if (trad === 'Manufacturer') {
             seg.forEach(function (seg) {
                 var segment = element(by.xpath('//md-radio-button[.//label[text()=\'' + seg + '\']]'));
@@ -147,13 +147,16 @@ describe('Sign up', function () {
             seg.forEach(function (seg) {
                 if (seg !== 'Rice' && seg !== 'Coffee' && seg !== 'Textiles' && seg !== 'Fruits and Vegetables') {
                     var showMore = element(by.xpath('//button[@aria-label=\'Switch1\']'));
-                    if(flag)
+                    if (flag)
                         showMore.click();
                     flag = 0;
                     if (subseg) {
                         var showDetails = element(by.xpath('//label[text()=\'Show Details\' and ..//label[text()=\'' + seg + '\']]'));
-                        showDetails.click();
-                        var subsegData=getsubSegmentData(seg,subseg);
+                        showDet
+
+
+                        ails.click();
+                        var subsegData = getsubSegmentData(seg, subseg);
                         for (var i = 0; i < subsegData.length; i++) {
                             var subSegment = element(by.xpath('//md-checkbox[@ng-model=\'node.isChecked\' and .//label[text()=\'' + subsegData[i] + '\']]'));
                             subSegment.click();
@@ -177,7 +180,7 @@ describe('Sign up', function () {
                 if (subseg) {
                     var showDetails = element(by.xpath('//label[text()=\'Show Details\' and ..//label[text()=\'' + seg + '\']]'));
                     showDetails.click();
-                    var subsegData=getsubSegmentData(seg,subseg);
+                    var subsegData = getsubSegmentData(seg, subseg);
                     for (var i = 0; i < subsegData.length; i++) {
                         var subSegment = element(by.xpath('//md-checkbox[@ng-model=\'node.isChecked\' and .//label[text()=\'' + subsegData[i] + '\']]'));
                         subSegment.click();
@@ -192,37 +195,37 @@ describe('Sign up', function () {
         }
     }
 
-    function generateOTPFunction(Username,Password,done){
+    function generateOTPFunction(Username, Password, done) {
 
-        if(signup.validateUserRegisteredData(Username,Password)) {
+        if (signup.validateUserRegisteredData(Username, Password)) {
 
             username.sendKeys(Username);
             password.sendKeys(Password);
 
             username.getAttribute('aria-invalid').then(function (attr) {
-                if(attr === 'false'){
+                if (attr === 'false') {
                     generateOTP.click();
                     var already = browser.findElement(by.xpath('//p[@data-ng-bind=\'registererror\' and ../../../../../@aria-hidden=\'false\']'));
                     already.getText().then(function (text) {
                         var txt = text;
                         if (txt === 'User is already Registered' || txt === 'Not allow to register. Please contact customer care.')
-                            done(txt,generateOTP);
-                        else if(txt === 'User is already verified :'+Username)
-                            done(txt,null);
+                            done(txt, generateOTP);
+                        else if (txt === 'User is already verified :' + Username)
+                            done(txt, null);
                         else
-                            done(null,null);
+                            done(null, null);
                     });
                 }
                 else
-                    done("Invalid Username",username);
+                    done("Invalid Username", username);
             });
 
         }
         else
-            done("Missing Username / Password",null);
+            done("Missing Username / Password", null);
     }
 
-    function verifyOTPFunction(){
+    function verifyOTPFunction() {
         var text = element(by.xpath('//p[contains(text(),\'An OTP has been sent\')]')).getText().then(function (sp) {
             return sp.split(". ");
         });
@@ -233,54 +236,54 @@ describe('Sign up', function () {
         generateOTP.click();
     }
 
-    function tradingFunction(Trading,done){
+    function tradingFunction(Trading, done) {
         var trading = element(by.xpath('//md-radio-button[.//h4[text()=\'' + Trading + '\']]'));
-        if(Trading) {
-            signup.isClickable(trading,function (error,ele) {
-                if(ele){
+        if (Trading) {
+            signup.isClickable(trading, function (error, ele) {
+                if (ele) {
                     trading.click();
                     nxt.click();
-                    done(null,null);
+                    done(null, null);
                 }
                 else
-                    done("Invalid Trading",trading);
+                    done("Invalid Trading", trading);
             });
         }
         else
-            done("Missing Trading",trading);
+            done("Missing Trading", trading);
     }
 
-    function segmentFunction(Trading,Segment,SubSegment,done){
-        if(validateSegment(Trading,Segment,SubSegment) === false){
-            done("Invalid Segment Details",null);
+    function segmentFunction(Trading, Segment, SubSegment, done) {
+        if (validateSegment(Trading, Segment, SubSegment) === false) {
+            done("Invalid Segment Details", null);
             return;
         }
 
-        selectSegment(Trading,Segment,SubSegment);
+        selectSegment(Trading, Segment, SubSegment);
         start.click();
-        done(null,null);
+        done(null, null);
     }
 
-    data.forEach(function(data){
+    data.forEach(function (data) {
         it('should register user', function () {
 
-            generateOTPFunction(data.username,data.password,function (error,ele) {
-                if(error === 'User is already verified :'+data.username);
-                else if(error){
+            generateOTPFunction(data.username, data.password, function (error, ele) {
+                if (error === 'User is already verified :' + data.username);
+                else if (error) {
                     console.log(error);
                     return;
                 }
                 else
                     verifyOTPFunction();
 
-                tradingFunction(data.trading,function (error,ele) {
-                    if(error){
+                tradingFunction(data.trading, function (error, ele) {
+                    if (error) {
                         console.log(error);
                         return;
                     }
 
-                    segmentFunction(data.trading,data.segment,data.subSegment,function (error,ele) {
-                        if(error){
+                    segmentFunction(data.trading, data.segment, data.subSegment, function (error, ele) {
+                        if (error) {
                             console.log(error);
                             return;
                         }
