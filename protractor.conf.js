39ae980569e94c7cfff71386fbc4f246d2cd7f09
aravindpanil <aravindpanil@gmail.com>
2018-07-11 12:36:58 +0530

    Integrate all modules of account into conf file

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,27 +8,10 @@ var config = {
         browser.driver.manage().window().maximize();
     },
     suites: {
-        registration: ['ee/ee/account/signup/*.js',
-            'ee/ee/account/signin/*.js'],
-        contacts: ['e2e/contacts/createcontact/*.js*',
-            'e2e/contacts/editcontact/*.js*',
-            'e2e/contacts/creategroup/*.js*'],
-        products: ['e2e/products/createproduct/*.js',
-            'e2e/products/editproduct/*.js',
-            'e2e/products/importproducts/*.js'],
-        all: ['e2e/account/signup/*.js',
-            'e2e/account/signin/*.js',
-            'e2e/products/createproduct/*.js',
-            'e2e/products/editproduct/*.js',
-            'e2e/products/importproducts/*.js',
-            'e2e/contacts/createcontact/*.js*',
-            'e2e/contacts/editcontact/*.js*',
-            'e2e/contacts/creategroup/*.js*',
-            'e2e/businessuser/createbusinessuser/*.js*',
-            'e2e/businessuser/editbusinessuser/*.js*',
-            'e2e/businessunit/createbusinessunit/*.js*',
-            'e2e/businessunit/editbusinessunit/*.js*',
-            'e2e/updatecompanyinfo/*.js*']
+        registration: ['ee/ee/account/signup/signup.js',
+            'ee/ee/account/signin/signin.js',
+            'ee/ee/account/signin/forgotpassword.js'
+        ],
     },
     jasmineNodeOpts: {
         isVerbose: true,
